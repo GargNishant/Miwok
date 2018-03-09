ed2ec52318d12056cc71f4258739e9b6a1862c87
Nishant <nishantgarg76342@gmail.com>
2018-03-09 15:58:06 +0530

    Added Tabs

--- app/src/main/java/com/example/nishantgarg/miwok/Activities/MainActivity.java
+++ app/src/main/java/com/example/nishantgarg/miwok/Activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nishantgarg.miwok.Activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,9 @@ import com.example.nishantgarg.miwok.Helper.FragmentHelper;
 import com.example.nishantgarg.miwok.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final  String[] fragmentName = {"Numbers",
+    "Family","Color","Phrases"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentHelper helper =new FragmentHelper(getSupportFragmentManager());
         viewPager.setAdapter(helper);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        for(int i=0; i<4; i++){
+            tabLayout.getTabAt(i).setText(fragmentName[i]);
+        }
     }
 }
